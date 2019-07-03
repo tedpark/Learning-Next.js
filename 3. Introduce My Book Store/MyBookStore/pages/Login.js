74d0ca561ae070207b3c7e90c9371b538a9d98b6
Ted Park <itstedpark@gmail.com>
2019-07-03 20:18:18 +0900

    add login form

--- 3. Introduce My Book Store/MyBookStore/pages/Login.js
+++ 3. Introduce My Book Store/MyBookStore/pages/Login.js
@@ -7,19 +7,54 @@ import {
   Column,
   Button,
   Hero,
-  Title
+  Title,
+  Field,
+  Control,
+  Input,
+  Icon,
+  Section,
+  Box
 } from "rbx";
 import "rbx/index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUsers,
+  faGamepad,
+  faEnvelope,
+  faCheck,
+  faLock
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Login() {
   return (
-    <Hero color={"primary"} size="fullheight" gradient>
+    <Hero>
       <Hero.Body>
         <Container>
-          <Title>{"info"} Login</Title>
-          <Title as="h2" subtitle>
-            {"info"} Login
-          </Title>
+          <Field>
+            <Control iconLeft iconRight>
+              <Input type="email" placeholder="Email" />
+              <Icon size="small" align="left">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </Icon>
+              <Icon size="small" align="right">
+                <FontAwesomeIcon icon={faCheck} />
+              </Icon>
+            </Control>
+          </Field>
+          <Field>
+            <Control iconLeft>
+              <Input type="password" placeholder="Password" />
+              <Icon size="small" align="left">
+                <FontAwesomeIcon icon={faLock} />
+              </Icon>
+            </Control>
+          </Field>
+          <Field>
+            <Control>
+              <Button color="success">Login</Button>
+            </Control>
+          </Field>
         </Container>
       </Hero.Body>
     </Hero>
