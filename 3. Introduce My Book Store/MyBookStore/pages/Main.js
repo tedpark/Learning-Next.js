4e02b9c88b660d25bd62211bedebf65a9b0b1c8c
ted park <itstedpark@gmail.com>
2019-07-04 00:21:15 +0900

    add kanban

--- 3. Introduce My Book Store/MyBookStore/pages/Main.js
+++ 3. Introduce My Book Store/MyBookStore/pages/Main.js
@@ -10,18 +10,101 @@ import {
   Title
 } from "rbx";
 import "rbx/index.css";
+//react-beautiful-dnd
+// 이거 한번 이식 해보는 걸로 하자.
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+// fake data generator
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
 
 export default function Main() {
   return (
     <Hero color={"info"} size="fullheight" gradient>
       <Hero.Body>
         <Container>
-          <Title>{"info"} Main</Title>
-          <Title as="h2" subtitle>
-            {"info"} Main
-          </Title>
+          <DragDropContext>
+            <Droppable droppableId="droppable">
+              {(provided, snapshot) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                >
+                  {getItems(10).map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          {item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
         </Container>
       </Hero.Body>
     </Hero>
   );
+}
+
+{
+  /* <Hero color={"info"} size="fullheight" gradient>
+<Hero.Body>
+  <Container>
+    <Title>{"info"} Main</Title>
+    <Title as="h2" subtitle>
+      {"info"} Main
+    </Title>
+  </Container>
+</Hero.Body>
+</Hero> */
 }
